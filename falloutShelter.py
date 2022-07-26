a029d486b9d0ff3048cd86dc03e01d96fb9f581d
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-07-26 11:02:11 +0530

    Create falloutShelter.py

---
+++ falloutShelter.py
@@ -0,0 +1,29 @@
+#fallout shelter logo in Python
+from turtle import *
+
+speed(0)
+bgcolor("gold")
+
+# Black circle
+penup()
+goto(0, -225)
+pendown()
+color("black")
+begin_fill()
+circle(225)
+end_fill()
+
+# Triangles
+penup()
+goto(0, 0)
+pendown()
+color("gold")
+begin_fill()
+for i in range(3):
+    right(120)
+    for i in range(3):
+        forward(200)
+        right(120)
+end_fill()
+
+hideturtle()
